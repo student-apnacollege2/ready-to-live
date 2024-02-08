e24ee6eb9f840bd1f0f9a7fb6856dd51ec8a7300
student-apnacollege2 <pradyumnakalaskar43@gmail.com>
2024-02-08 23:29:29 +0530

    first commit

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import ErrorHandler from "../utils/errorHandler.js";
+import User from "../models/user.js";
+import catchAsyncErrors from "./catchAsyncErrors.js";
+
+// Checks if user is Authenticated or not
+export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorHandler("Login first to access this resource", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decoded.id);
+
+  next();
+});
+
+// Authorize user roles
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role (${req.user.role}) is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+
+    next();
+  };
+};
---
+++ backend/seeder/seeder.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import products from "./data.js";
+import Product from "../models/product.js";
+
+const seedProducts = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://pradyumnakalaskar43:MH31JO9890@cluster0.5zneo3w.mongodb.net/shopit-v2?retryWrites=true&w=majority"
+    );
+
+    await Product.deleteMany();
+    console.log("Products are deleted");
+
+    await Product.insertMany(products);
+    console.log("Products are added");
+
+    process.exit();
+  } catch (error) {
+    console.log(error.message);
+    process.exit();
+  }
+};
+
+seedProducts();
